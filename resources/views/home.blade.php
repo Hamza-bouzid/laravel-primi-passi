57ee9db679f0b35a7456e8203946b0257feab210
Hamza Bouzid <hamzadeveloper1@gmail.com>
2022-03-02 14:42:30 +0100

    end

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Hello World</title>
+
+  <style>
+    body {
+      background-color: rgba(0, 0, 0, 0.9);
+      color: #fff
+    }
+
+    section {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    }
+
+    h1 {
+      font-size: 200px;
+    }
+  </style>
+</head>
+<body>
+  <section>
+    <h1>Hello World</h1>
+  </section>
+  
+</body>
+</html>
